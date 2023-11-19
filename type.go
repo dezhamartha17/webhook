3d944d220b4c4f1a8174cc0ff1006fe71df974b0
dezhamartha17 <dezhamartha17@gmail.com>
2023-11-19 21:16:07 +0700

    first commit

---
+++ type.go
@@ -0,0 +1,13 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"ada apa nih rame-rame?",
+	"ada niatan apaan kak?",
+	"mohon bersabar ya kaks",
+	"silahkan bagikan lokasi kakak nya, biar di jemput",
+	"kayanya mimin lagi afk nih",
+}
